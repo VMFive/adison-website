496667d8dce3b394ebd39a6f8f632b2c848780b0
John Lee <johnteee@gmail.com>
2019-10-25 18:31:42 +0800

    Add Video/Image Mapping

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -24,3 +24,16 @@ var slideDemo = new Siema({
   onInit: () => {},
   onChange: () => {},
 });
+
+let videos = {
+  "Motion-Ads":"https://adison.com.tw/wp-content/uploads/2018/12/1.軸感互動（食品）：麥當勞搖搖雞球_edit改.mp4",
+  "Dynamic-Interstitial":"https://adison.com.tw/wp-content/uploads/2018/12/2.動態蓋板（食品）：肯德基_edit改.mp4",
+  "Dynamic-In-Read-Ads":"https://adison.com.tw/wp-content/uploads/2018/12/3.動態文中：Swift_edit改.mp4",
+  "Carousel-Ads":"https://adison.com.tw/wp-content/uploads/2018/12/4.輪播式（車）：Mazda_edit改.mp4",
+  "Impressive-Ads":"https://adison.com.tw/wp-content/uploads/2018/12/5.滾動式：溫徹斯特鬼屋_edit改.mp4",
+  "Vertical-Ads":"https://adison.com.tw/wp-content/uploads/2018/12/6.直立式（車）：Lexus_edit改.mp4",
+  "Interstitial-Ads":"https://adison.com.tw/wp-content/uploads/2018/12/7.蓋版（車）：瑪莎拉蒂_edit改.mp4",
+  "Native-Flip-Ads":"https://adison.com.tw/wp-content/uploads/2018/12/8.原生翻轉Banner（電影）：分秒幣爭_edit改.mp4",
+  "Native-Ads":"https://adison.com.tw/wp-content/uploads/2018/12/9.原生：Uniqlo_edit改.mp4",
+  "Top-Ads":"https://adison.com.tw/wp-content/uploads/2018/12/10.原生Top（音樂）：環球泰勒絲_edit改.mp4",
+}
